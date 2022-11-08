62c966d33954721558148b8b0336b8a28b94ea1f
Nekodoll <37606008+Nekodoll@users.noreply.github.com>
2022-11-08 10:19:34 +0700

    Update bot.go

--- cryptofy/bot.go
+++ cryptofy/bot.go
@@ -14,11 +14,12 @@ func main() {
 	getURL := "https://cryptofy.club/home.php?LTC=1&DOGE=1&DGB=1&TRX=1&USDT=1&BCH=1&DASH=1&FEY=1&ZEC=1&SOL=1&redirect=1"
 	data := url.Values{}
 	getHeader := "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
+	cookie := "PHPSESSID=9bf0c1fb842a76b8914a0da2b4293f13; _immortal|Arc_nodeId=YFbrRdDoJHPU8htBtRTyfe; widgetOptState={%22state%22:%22UNDECIDED%22%2C%22date%22:%222022-11-08T03:16:13.186Z%22%2C%22dismissedAt%22:null}"
 	client := &http.Client{}
 	request, _ := http.NewRequest(http.MethodPost, getURL, strings.NewReader(data.Encode()))
 	request.Header.Add("User-Agent", getHeader)
 	request.Header.Add("content-type", "application/x-www-form-urlencoded")
-	request.Header.Add("Cookie", "PHPSESSID=d8230d64aba3447e0157de1bd077fe43; _immortal|Arc_nodeId=JvxN5ac7gBV9THPxnDxbWj; widgetOptState={%22state%22:%22UNDECIDED%22%2C%22date%22:%222022-11-04T08:16:20.606Z%22%2C%22dismissedAt%22:null}")
+	request.Header.Add("Cookie", cookie)
 	for {
 		resp, _ := client.Do(request)
 		if resp.StatusCode == 200 {
