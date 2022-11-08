df31eeb67c649c9acfd81c6971f1a839e8d70805
Nekodoll <37606008+Nekodoll@users.noreply.github.com>
2022-11-08 10:49:55 +0700

    Update bot.go

--- cryptofy/bot.go
+++ cryptofy/bot.go
@@ -9,9 +9,9 @@ import (
 )
 
 func main() {
-
+	
 	colorGreen := "\033[32m"
-	getURL := "https://cryptofy.club/home.php?LTC=1&DOGE=1&DGB=1&TRX=1&USDT=1&BCH=1&DASH=1&FEY=1&ZEC=1&SOL=1&redirect=1"
+	getURL := "https://cryptoshi.club/home.php?LTC=1&DOGE=1&DGB=1&TRX=1&USDT=1&BCH=1&DASH=1&FEY=1&ZEC=1&SOL=1&redirect=1"
 	data := url.Values{}
 	getHeader := "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
 	cookie := "PHPSESSID=5958157c25ce0187d81746fa4ac1e87c; _immortal|Arc_nodeId=RzC88WkaJf2TH7R4L6tZeE; widgetOptState={%22state%22:%22UNDECIDED%22%2C%22date%22:%222022-10-22T07:09:14.153Z%22%2C%22dismissedAt%22:null}; _ga=GA1.2.1160819856.1666422601; _gid=GA1.2.1177212240.1667878780; _gat_gtag_UA_193127892_1=1"
